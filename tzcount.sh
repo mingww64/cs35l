e620977c0988245825d41bc1f690c6674f3f6a54
mingww64 <i@feli.cf>
2025-11-04 01:20:49 +0000

    update tz

--- tzcount.sh
+++ tzcount.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 set -euo pipefail
-git log "$1" |grep -Po "(?<=Date:.{28})[+-]{1}\d{4}$" |sort|uniq -c|awk '{print $2, $1}'|sort -n #>  tzdb-"${1//../-}".tzcount
+git log "$1" --pretty=fuller|grep -Po "(?<=CommitDate:.{25})[+-]{1}\d{4}$" |sort|uniq -c|awk '{print $2, $1}'|sort -n #>  tzdb-"${1//../-}".tzcount
